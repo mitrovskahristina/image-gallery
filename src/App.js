34708fcc18ac0bf079a53385dc6331f828a80fd6
mitrovskahristina <hristinamitrovska04@gmail.com>
2023-02-28 14:44:25 +0100

    Remove unused component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Gallery from './components/Gallery';
-import ImageGallaryComponent from './components/ImageGalleryComponent';
 import items from './components/items'
 
 function App() {
   return (
     <div className="App">
       <Gallery items={items} />
-      {/* <ImageGallaryComponent /> */}
     </div>
   );
 }
